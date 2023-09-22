cb49d1ce6bc5fd432d4f1620334bc4b72852cef0
wuxiaohui9707 <wuxiaohui9707@vt.edu>
2023-09-22 01:43:05 -0400

    ''

---
+++ label.py
@@ -0,0 +1,9 @@
+from RTTM_Transform import RTTM_transform
+
+if __name__ == "__main__":
+    # 在模块脚本中添加 __main__ 部分，以便该脚本可以作为独立程序运行
+    input_file = "E:/Files/Acoustic Data/DSFAS_Chen/Fig4_Eructation_1.txt"
+    output_file = "E:/Files/Acoustic Data/DSFAS_Chen/Fig4_Eructation_1.rttm"
+    RTTM_transform(input_file,output_file)
+
+
