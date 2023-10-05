ffff2f0c5ed816c77dde200d23439ff079f71b40
wuxiaohui9707 <wuxiaohui9707@vt.edu>
2023-10-05 10:48:47 -0400

    update

--- Split_Audio.py
+++ Split_Audio.py
@@ -11,5 +11,5 @@ def split_audio_using_segments(audio_file, segments_file, output_folder):
             start_time = float(parts[0])
             end_time = float(parts[1])
             label = parts[2] 
-            segment = audio[start_time * 1000:end_time * 1000]  # pydub 使用毫秒作为单位
+            segment = audio[start_time * 1000:end_time * 1000]  #pydub使用毫秒作为单位
             segment.export(os.path.join(output_folder, f"segment_{i}.wav"), format="wav")
